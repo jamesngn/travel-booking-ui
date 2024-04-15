fe2b6f422f78cabb96323c145e5799e1dc249b9a
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-15 10:40:03 +1000

    Refactor hotel page and app layout components

--- src/pages/hotel.page/index.tsx
+++ src/pages/hotel.page/index.tsx
@@ -2,28 +2,36 @@ import { useGetListHotelQuery } from "../../api/hooks/hotel.hook";
 import React from "react";
 import HotelCardList from "./components/hotel-card-list";
 import { useLocation, useParams } from "react-router-dom";
+import Loading from "@/components/Loading";
+import { Box } from "@mantine/core";
 
 const HotelPage: React.FC = () => {
-  //hotel?location=${values.location} extract location from the link
-
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-
   const hotelLocation = searchParams.get("location");
 
   const { isLoading, data } = useGetListHotelQuery({
     location: hotelLocation || "",
   });
 
+  const renderHotels = () => {
+    if (isLoading) {
+      return <Loading isLoading={isLoading} />;
+    }
+
+    if (data && data.length === 0) {
+      return <div>No hotels found</div>;
+    }
+
+    return <HotelCardList hotelList={data} />;
+  };
+
   return (
     <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <HotelCardList hotelList={data} />
-        </>
-      )}
+      <h1>Hotels</h1>
+      <Box pos={"relative"} mih={200}>
+        {renderHotels()}
+      </Box>
     </div>
   );
 };
--- src/shared/components/layout/app-layout.tsx
+++ src/shared/components/layout/app-layout.tsx
@@ -1,16 +1,73 @@
-import React, { PropsWithChildren } from "react";
+import HomeButton from "@/components/HomeButton/HomeButton";
+import MyBookingBtn from "@/components/MyBookingButton/HomeButton";
+import LogoutButton from "@/shared/services/auth/logout";
+import { Box, Button, Text } from "@mantine/core";
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
   return (
     <div>
       {/* Header */}
-      <header>Travel App</header>
+      <header
+        style={{
+          borderBottom: "1px solid #ddd",
+          padding: "20px",
+        }}
+      >
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          {!isLoginPage ? <HomeButton /> : <div></div>}
+          <h1> Travel App </h1>
+          {!isLoginPage ? (
+            <div
+              style={{
+                display: "flex",
+                gap: "20px",
+              }}
+            >
+              <MyBookingBtn />
+              <LogoutButton />
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </Box>
+      </header>
 
       {/* Main content */}
-      <main style={{ margin: "0 120px" }}>{children}</main>
+      <main
+        style={{
+          margin: "0 120px",
+          height: "calc(100vh - 160px)",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {children}
+      </main>
 
       {/* Footer */}
-      <footer>
+      <footer
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          padding: "20px",
+          backgroundColor: "#f5f5f5",
+          textAlign: "center",
+        }}
+      >
         {/* Add your footer content here */}
         Travel App &copy; 2024
       </footer>
