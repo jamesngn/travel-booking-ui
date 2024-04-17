1a881c5dafd2e22d717e2e242e57456551fe0601
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-17 10:45:51 +1000

    Update room reservation functionality in RoomCardList component

--- src/pages/hotel-details.page/components/room-card-list/index.tsx
+++ src/pages/hotel-details.page/components/room-card-list/index.tsx
@@ -12,6 +12,8 @@ import {
 import { useReserveRoomMutation } from "@/api/hooks/hotel.hook";
 import { IReserveRoomQuery } from "@/api/hotel";
 import { notifications } from "@mantine/notifications";
+import { set } from "lodash";
+import Auth from "@/shared/services/auth";
 
 const RoomCardList: React.FC<{ data: IRoomTypeData[] }> = ({ data }) => {
   const roomSelection = useRoomSelectionManagementStore(
@@ -35,29 +37,33 @@ const RoomCardList: React.FC<{ data: IRoomTypeData[] }> = ({ data }) => {
 
   const navigate = useNavigate();
 
+  const userId = Auth.token || null;
+  const [loading, setLoading] = React.useState(false);
+
   const createHotelBooking = useReserveRoomMutation({
     onSuccess: () => {
-      console.log("success");
+      setLoading(false);
       navigate("/success");
+      notifications.show({
+        title: "Success",
+        message: "Room reserved successfully",
+        color: "green",
+      });
     },
     onError: () => {
-      notifications.show({
-        title: "Error",
-        message: "An error occurred while reserving the room",
-        color: "red",
-      });
+      setLoading(false);
     },
   });
 
   const onReserveRoom = () => {
     const reserveRoomRequest: IReserveRoomQuery = {
-      bookingId: 1,
       hotelId: Number(id),
-      userId: 2,
+      userId: Number(userId),
       checkInDate,
       checkOutDate,
       rooms: roomSelection,
     };
+    setLoading(true);
 
     createHotelBooking.mutate(reserveRoomRequest);
   };
@@ -65,9 +71,22 @@ const RoomCardList: React.FC<{ data: IRoomTypeData[] }> = ({ data }) => {
   return (
     <Box
       className="hotel-list-ctn"
-      style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
     >
-      <Box style={{ maxWidth: "1400px" }}>
+      <Box
+        style={{
+          maxWidth: "1400px",
+          overflowY: "scroll",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          height: "calc(50vh)",
+        }}
+      >
         {data.map((room, index) => (
           <RoomDetailCard
             key={index}
@@ -85,7 +104,12 @@ const RoomCardList: React.FC<{ data: IRoomTypeData[] }> = ({ data }) => {
         ))}
       </Box>
 
-      <Button disabled={isDisabled} fullWidth onClick={onReserveRoom}>
+      <Button
+        disabled={isDisabled}
+        fullWidth
+        onClick={onReserveRoom}
+        loading={loading}
+      >
         I'll Reserve
       </Button>
     </Box>
